411f5c375693f82145d065fc501b5613c3a45011
Daniel Bailo <danbailoufms@gmail.com>
2020-11-11 01:08:10 -0300

    created a method to init multitracker and the procedure to retracking objects using the time of video

--- src/multitracker.py
+++ src/multitracker.py
@@ -2,6 +2,16 @@ import cv2
 from core.crop import Crop
 import imutils
 
+def init_tracker(bboxes, frame):
+    # Create MultiTracker object
+    multiTracker = cv2.MultiTracker_create()
+
+    # Initialize MultiTracker 
+    for bbox in bboxes:
+        multiTracker.add(cv2.TrackerCSRT_create(), frame, bbox)
+
+    return multiTracker
+
 if __name__ == "__main__":
 
     cap = cv2.VideoCapture("videos/race.mp4")
@@ -14,24 +24,46 @@ if __name__ == "__main__":
 
     crop.update_time_video(time_to_start)
    
-    bboxes, init_frame = crop.select_bboxes()
+    bboxes, frame = crop.select_bboxes()
 
-    # Create MultiTracker object
-    multiTracker = cv2.MultiTracker_create()
+    multiTracker = init_tracker(bboxes=bboxes, frame=frame)
 
-    # Initialize MultiTracker 
-    for bbox in bboxes:
-        multiTracker.add(cv2.TrackerCSRT_create(), init_frame, bbox)
+    while True:
 
-    while cap.isOpened():
-        retval, frame = cap.read()
+        retval, frame = crop.video.read()
+
+        # end of video
         if not retval:
-            break		
-        
+            break
+
         #resize the frame
         if crop.resize["bool"]:
-            frame = imutils.resize(frame, width=crop.resize["width"])
+            frame = imutils.resize(frame, width=crop.resize["width"])        
 
+        if crop.get_current_time() >= time_to_end:
+            cv2.destroyAllWindows()
+
+            if crop.extract_more():
+              
+                while True:
+                    time_to_start = crop.set_start_video()
+                    if time_to_start > crop.get_current_time():
+                        break
+
+                crop.update_time_video(time_to_start)
+                
+                while True:
+                    time_to_end = crop.set_end_video()
+                    if time_to_end > time_to_start:
+                        break
+                pad = crop.add_pad()
+
+                bboxes, frame = crop.select_bboxes()
+                multiTracker = init_tracker(bboxes=bboxes, frame=frame)
+
+            else: 
+                break
+      
         success, boxes = multiTracker.update(frame)
 
         for i, newbox in enumerate(boxes):
@@ -41,8 +73,8 @@ if __name__ == "__main__":
             cv2.rectangle(frame, p1, p2, crop.colors[i], 2)
 
         # show frame        
-        cv2.imshow('MultiTracker', frame)
+        cv2.imshow('IC - D&D', frame)
         
-        # quit on ESC button or q button
+        # quit on ESC button
         if cv2.waitKey(1) & 0xFF == 27:  # Esc pressed
             break
