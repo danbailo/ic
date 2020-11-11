10d632cfd073fe83faa203c4a90a8baa5e1fa670
Daniel Bailo <danbailoufms@gmail.com>
2020-11-11 04:24:26 -0300

    implemented the crop of all trackers and slices, added releases of objects

--- src/multitracker.py
+++ src/multitracker.py
@@ -5,9 +5,7 @@ import os
 
 # TO DO
 # test the pad - OK
-# crop images, save each group in a folder
-#
-#
+# crop images, save each group in a folder - OK
 
 if __name__ == "__main__":
     Crop.delete_imgs()
@@ -28,13 +26,10 @@ if __name__ == "__main__":
     pad = crop.add_pad()
 
     crop.update_time_video(time_to_start)
-   
     bboxes, frame = crop.select_bboxes()
-
     multiTracker = crop.init_tracker(bboxes=bboxes, frame=frame)
 
-    img_n = 0
-
+    names_of_frames = {}
     while True:
 
         retval, frame = crop.video.read()
@@ -66,9 +61,9 @@ if __name__ == "__main__":
                 pad = crop.add_pad()
 
                 bboxes, frame = crop.select_bboxes()                
-                crop.n_of_slices += 1
+                crop.n_of_slices += 1                
                 multiTracker = crop.init_tracker(bboxes=bboxes, frame=frame)
-
+                names_of_frames.clear()
             else: 
                 break
       
@@ -83,15 +78,22 @@ if __name__ == "__main__":
             p2_pad = (p2[0] + pad, p2[1] + pad)
         
             ROI = frame[y-pad:y+h+pad, x-pad:x+w+pad]
-            crop.create_img_folder()
-            # cv2.imwrite(f'images/frame_{img_n}.jpg', ROI)
-            img_n += 1
+            crop.create_img_folder()         
+
+            if not names_of_frames.get(i):
+                names_of_frames[i] = 1
+
+            crop.crop_img(ROI, i, names_of_frames)
+            names_of_frames[i] += 1
             
             cv2.rectangle(frame, p1_pad, p2_pad, crop.colors[i], 2)
 
-        # show frame        
         cv2.imshow('IC - D&D', frame)
         
-        # quit on ESC button
+        # close
         if cv2.waitKey(1) & 0xFF == 27:  # Esc pressed
             break
+
+    crop.video.release()
+    cap.release()
+    cv2.destroyAllWindows()
